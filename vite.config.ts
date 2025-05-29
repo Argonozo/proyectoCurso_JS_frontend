7c84802906da2abbe09e84e5cfb8b267bff4a230
Argonozo <elmecaniqueador@gmail.com>
2025-05-29 11:20:48 -0400

    Fix: Ensure base path is set for deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Asegura que la base de la aplicación sea la raíz para el despliegue
   plugins: [
     vue(),
     vueDevTools(),
